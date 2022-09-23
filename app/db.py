f1d005ba9a366a37a76a4ed51c088816a5d8cd9e
abir101 <abir101@debian-BULLSEYE-live-builder-AMD64>
2022-09-23 21:17:51 +0530

    master

--- app/db.py
+++ app/db.py
@@ -3,6 +3,6 @@ import os
 
 
 def db_connection():
-    uri = os.environ.get("DB_URI") or 'postgres://abir101:ticketsAppdb@abir101.mysql.pythonanywhere-services.com/ticketsApp'
+    uri = os.environ.get("DB_URI") or 'postgres://abir101:ticketsAppdb@abir101.mysql.pythonanywhere-services.com/abir$ticketsApp'
     con = psycopg2.connect(uri)
     return con

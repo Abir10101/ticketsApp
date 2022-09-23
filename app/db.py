2d995187eaa43141e72080966afd3c5d705fc5ff
abir101 <abir101@debian-BULLSEYE-live-builder-AMD64>
2022-09-23 22:45:01 +0530

    master

--- app/db.py
+++ app/db.py
@@ -1,8 +1,13 @@
-import psycopg2
+from flask_mysql_connector import MySQL
+from app import app
 import os
 
 
 def db_connection():
-    uri = os.environ.get("DB_URI") or 'postgres://abir101:ticketsAppdb@abir101.mysql.pythonanywhere-services.com/abir$ticketsApp'
-    con = psycopg2.connect(uri)
-    return con
+    # uri = os.environ.get("DB_URI") or 'mysql://abir101:ticketsAppdb@abir101.mysql.pythonanywhere-services.com/abir$ticketsApp'
+    app.config['MYSQL_USER'] = 'abir101'
+    app.config['MYSQL_DATABASE'] = 'abir$ticketsApp'
+    app.config['MYSQL_HOST'] = 'abir101.mysql.pythonanywhere-services.com'
+    app.config['MYSQL_PASSWORD'] = 'ticketsAppdb'
+    mysql = MySQL(app)
+    return mysql.connection

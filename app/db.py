8d4cfc371c8c37f0ed8a67a142fc3e1eeb003781
abir101 <abir101@debian-BULLSEYE-live-builder-AMD64>
2022-09-23 23:03:02 +0530

    master

--- app/db.py
+++ app/db.py
@@ -1,13 +1,8 @@
-from flask_mysql_connector import MySQL
-from app import app
+import pymysql
 import os
 
 
 def db_connection():
     # uri = os.environ.get("DB_URI") or 'mysql://abir101:ticketsAppdb@abir101.mysql.pythonanywhere-services.com/abir$ticketsApp'
-    app.config['MYSQL_USER'] = 'abir101'
-    app.config['MYSQL_DATABASE'] = 'abir$ticketsApp'
-    app.config['MYSQL_HOST'] = 'abir101.mysql.pythonanywhere-services.com'
-    app.config['MYSQL_PASSWORD'] = 'ticketsAppdb'
-    mysql = MySQL(app)
-    return mysql.connection
+    con = pymysql.connect(host="abir101.mysql.pythonanywhere-services.com",user="abir101",password="ticketsAppdb",database="abir$ticketsApp" )
+    return con

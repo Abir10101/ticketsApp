57f476a478eb9fb0b499238b14d69c2443cf56b9
abir101 <abir101@debian-BULLSEYE-live-builder-AMD64>
2022-09-25 09:13:22 +0530

    added environment variables

--- app/config.py
+++ app/config.py
@@ -1,11 +1,12 @@
 import os
+from dotenv import load_dotenv
 
 
 class Config:
     # Database configuration
-    DB_HOST = 'sql6.freemysqlhosting.net'
-    DB_USER = 'sql6521869'
-    DB_PASSWORD = 'VtjDmWW1ed'
-    DB_NAME = 'sql6521869'
+    DB_HOST = os.environ.get('DB_HOST') or 'sql6.freemysqlhosting.net'
+    DB_USER = os.environ.get('DB_USER') or 'sql6521869'
+    DB_PASSWORD = os.environ.get('DB_PASSWORD') or 'VtjDmWW1ed'
+    DB_NAME = os.environ.get('DB_NAME') or 'sql6521869'
     # Secret Key for application
-    SECRET_KEY = 'asdf'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard-to-guess'

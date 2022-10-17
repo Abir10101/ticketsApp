363c9bc5a896a9df1330ac9e497f4f26aaf4aaae
abir101 <abir101@debian-BULLSEYE-live-builder-AMD64>
2022-10-17 19:29:03 +0530

    master

--- app/config.py
+++ app/config.py
@@ -4,9 +4,9 @@ from dotenv import load_dotenv
 
 class Config:
     # Database configuration
-    DB_HOST = os.environ.get('DB_HOST') or 'sql6.freemysqlhosting.net'
-    DB_USER = os.environ.get('DB_USER') or 'sql6522130'
-    DB_PASSWORD = os.environ.get('DB_PASSWORD') or 'JlJf8vElrz'
-    DB_NAME = os.environ.get('DB_NAME') or 'sql6522130'
+    DB_HOST = os.environ.get('DB_HOST') or 'sql8.freemysqlhosting.net'
+    DB_USER = os.environ.get('DB_USER') or 'sql8527240'
+    DB_PASSWORD = os.environ.get('DB_PASSWORD') or '7eejrj5UeH'
+    DB_NAME = os.environ.get('DB_NAME') or 'sql8527240'
     # Secret Key for application
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard-to-guess'
